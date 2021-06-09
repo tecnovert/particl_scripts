d2e894eed49edbb16601a8b54352a7bc0e0dea66
tecnovert <tecnovert@tecnovert.net>
2021-06-09 02:53:35 +0200

    New trace script to remove wallet names.
    Pull in unknown inputs from known chain data db.
    Insert values from the chain into the json trace data.

--- trace_frozen_inplace.py
+++ trace_frozen_inplace.py
@@ -14,12 +14,13 @@ Verifies
    - If anon spendkeys are provided.
  - Check claimed anon index is possible.
  - Check claimed anon indices are not reused.
-   - Write to file to cache across all runs
+   - Write to file to cache across all runs.
  - Keyimages are not reused.
+ - Unknown input amounts are feasible when compared against accumulated chain info.
 
 ~/tmp/particl-0.19.2.11/bin/particl-qt -txindex=1 -server -printtoconsole=0 -nodebuglogfile
 ./particl-cli -rpcwallet=wallet.dat debugwallet "{\"trace_frozen_outputs\":true}"  > ~/trace_wallets.txt
-$ python trace_frozen.py ~/.particl ~/trace_wallets.txt
+$ python trace_frozen_inplace.py ~/.particl ~/trace_wallets.txt
 
 """
 
@@ -27,11 +28,15 @@ import os
 import sys
 import json
 import time
+import sqlite3
 from util import callrpc, make_int, format8, b58decode, COIN
 from ecc_util import hashToCurve, pointToCPK, G, b2i, b2h
 
 persistent_data_file_in = os.getenv('PERSISTENT_DATA_FILE', '~/trace_frozen_data.json')
 persistent_data_file = os.path.expanduser(persistent_data_file_in)
+
+chain_info_db_file_in = os.getenv('CHAIN_INFO_DB_FILE', '~/v23_anon_stats/chain_stats.db')
+chain_info_db_file = os.path.expanduser(chain_info_db_file_in)
 
 
 def fromWIF(x):
@@ -61,7 +66,7 @@ def main():
     print('Core version', r['version'])
     print('Use anon spend keys', use_anon_spend_keys)
     print('Persistent data path', persistent_data_file_in)
-
+    print('Chain info DB path', chain_info_db_file_in)
 
     spent_anon_inputs = {}
     blacklisted_aos = []
@@ -76,27 +81,52 @@ def main():
     with open(input_file) as fp:
         input_json = json.load(fp)
 
+    try:
+        dbc = sqlite3.connect(chain_info_db_file)
+    except Exception as e:
+        print('chain_info_db_file not found', e)
+        dbc = None
+
     used_keyimages = set()
-
     inputs_map = {}
+    wallet_names = []
 
-    def trace_tx_inputs(itx, spending_txid, spending_tx, issues):
+    def replace_wallet_name(wallet_name):
+        replaced = []
+        for wm in wallet_name.split(', '):
+            try:
+                replaced.append('wallet_{}'.format(wallet_names.index(wm)))
+            except:
+                wallet_names.append(wm)
+                replaced.append('wallet_{}'.format(wallet_names.index(wm)))
+        return ', '.join(replaced)
+
+    def trace_tx_inputs(itx, spending_txid, spending_path, spending_tx, issues):
         #print(json.dumps(itx, indent=4))
         txid = itx['txid']
         tx = callrpc(rpc_port, rpc_auth, 'getrawtransaction', [txid, True])
+
+        w_name = replace_wallet_name(itx['wallet'])
+        itx['wallet'] = w_name
 
         if 'ct_fee' in tx['vout'][0]:
             ct_fee = tx['vout'][0]['ct_fee']
         else:
             ct_fee = 0
 
+        itx['fee'] = ct_fee
+
         total_in = 0
         total_out = make_int(ct_fee)
-        spent_out = 0  # Pass up to verify spending_txid
+        spent_out_values = []  # Pass up to verify spending_txid
 
+        repeated_inputs = False
+
+        tx_issues = []
         claimed_outputs = []
         outputs_by_type = {}
         known_output_values = {}
+        known_input_values = []
         # Verify the claimed output amounts
         for txo in tx['vout']:
             txo_type = txo['type']
@@ -122,7 +152,7 @@ def main():
                     except Exception as e:
                         warning = 'Warning: verifycommitment failed for output {} for tx {}.'.format(txo['n'], txid)
                         print(warning)
-                        issues.append(warning)
+                        tx_issues.append(warning)
                     found_vout = True
 
                     if txo_type == 'anon':
@@ -130,14 +160,14 @@ def main():
                         if anon_index in blacklisted_aos:
                             warning = 'Warning: Blacklisted anon output: {}.'.format(anon_index)
                             print(warning)
-                            issues.append(warning)
+                            tx_issues.append(warning)
 
                         pubkey = txo['pubkey']
                         ao_check = callrpc(rpc_port, rpc_auth, 'anonoutput', [pubkey])
                         assert(ao_check['index'] == anon_index)
 
                     if spending_txid is not None and 'spent_by' in txo_verify and spending_txid == txo_verify['spent_by']:
-                        spent_out += txo_verify['value']
+                        spent_out_values.append(txo_verify['value'])
                         # Verify anon_index is possible
                         if txo_type == 'anon':
                             anon_index = txo_verify['anon_index']
@@ -182,14 +212,14 @@ def main():
                 if found_vout is False:
                     warning = 'Warning: Missing output {} for tx {}.'.format(txo['n'], txid)
                     print(warning)
-                    issues.append(warning)
+                    tx_issues.append(warning)
             elif txo_type == 'standard':
                 total_out += txo['valueSat']
                 known_output_values[txo['n']] = (txo_type, txo['valueSat'])
             else:
                 warning = 'Warning: Unknown output type {} for tx {}.'.format(txo_type, txid)
                 print(warning)
-                issues.append(warning)
+                tx_issues.append(warning)
 
         if itx['input_type'] != 'plain' and 'inputs' in itx:
             tx_inputs = None
@@ -198,16 +228,20 @@ def main():
                     #tx_inputs = inputs_map[txid]
                     total_in += inputs_map[txid]
                     tx_inputs = []
+                    repeated_inputs = True
             else:
                 tx_inputs = itx['inputs']
 
             if tx_inputs is None:
                 warning = 'Warning: Missing inputs for tx {}.'.format(txid)
                 print(warning)
-                issues.append(warning)
+                tx_issues.append(warning)
             else:
                 for txi_verify in tx_inputs:
-                    total_in += trace_tx_inputs(txi_verify, txid, tx, issues)
+                    new_spending_path = txid + ('' if spending_path == '' else ' -> ') + spending_path
+                    input_values = trace_tx_inputs(txi_verify, txid, new_spending_path, tx, issues)
+                    total_in += sum(input_values)
+                    known_input_values += input_values
 
                 if txid not in inputs_map:
                     #inputs_map[txid] = tx_inputs
@@ -217,51 +251,109 @@ def main():
                 if 'type' in txin and txin['type'] != 'standard':
                     warning = 'Warning: Missing blinded inputs for tx {}.'.format(txid)
                     print(warning)
-                    issues.append(warning)
+                    tx_issues.append(warning)
                 else:
                     prev_tx = callrpc(rpc_port, rpc_auth, 'getrawtransaction', [txin['txid'], True])
                     prevout = prev_tx['vout'][txin['vout']]
                     if prevout['type'] != 'standard':
-                        warning = 'Warning: Missing blinded inputs for tx {}.'.format(txid)
+                        warning = 'Error: Missing plain inputs for tx {}.'.format(txid)
                         print(warning)
-                        issues.append(warning)
+                        tx_issues.append(warning)
+                        assert(False)
                     else:
                         total_in += prevout['valueSat']
+                        known_input_values.append(prevout['valueSat'])
 
         print('txid', txid)
         if spending_txid is not None:
-            print('\tinput for ', spending_txid)
+            #print('\tinput for', spending_txid)
+            print('\tinput for', spending_path)
+
         print('\ttotal_in', total_in)
         print('\ttotal_out', total_out)
 
+        itx['total_in'] = total_in
+        itx['total_out'] = total_out
 
         num_outputs = sum(outputs_by_type.values())
         num_known_outputs = len(known_output_values)
+
+        if repeated_inputs is False:
+            num_inputs = 0
+            for txin in tx['vin']:
+                if 'type' in txin and txin['type'] == 'anon':
+                    num_inputs += txin['num_inputs']
+                else:
+                    num_inputs += 1
+
+            if dbc is not None and len(known_input_values) < num_inputs:
+                cur = dbc.cursor()
+                total_possible_input = 0
+                for txin in tx['vin']:
+                    if 'type' in txin and txin['type'] == 'blind':
+                        cur.execute('SELECT value, is_estimate FROM outputs WHERE txid=? AND n=?', (txin['txid'], txin['vout']))
+                        total_possible_input += cur.fetchone()[0]
+                    elif 'type' in txin and txin['type'] == 'anon':
+                        print('MLSAG rows, cols:', txin['num_inputs'], txin['ring_size'])
+                        str_hdr = ''
+                        for i in range(txin['ring_size']):
+                            str_hdr += str(i).ljust(32)
+                        print(str_hdr)
+                        for i in range(txin['num_inputs']):
+                            ring_row = txin['ring_row_{}'.format(i)]
+                            ais = [int(i) for i in ring_row.split(', ')]
+                            cur.execute('SELECT value, is_estimate, anon_index, spent_txid FROM outputs WHERE anon_index IN ({})'.format(','.join(['?'] * len(ais))), (ais))
+                            aos = {}
+                            for r in cur.fetchall():
+                                ao_value, ao_is_estimate, aoi, ao_spent_txid = r
+                                ao_note = ''
+                                if ao_spent_txid is not None and ao_spent_txid != txid:
+                                    ao_note = 'S'  # Spent elsewhere
+                                if ao_is_estimate:
+                                    ao_note += 'E'
+                                aos[aoi] = (ao_value, ao_note)
+                            str_row = ''
+                            for c, ai in enumerate(ais):
+                                str_row += (str(aos[ai][0]) + aos[ai][1] + ' ' + str(ai)).ljust(32)
+                            print(str_row)
+
+                    else:
+                        num_inputs += 1
+                    cur.close()
+
+                print('\ttotal_possible_input', total_possible_input)
+                itx['total_possible_input'] = total_possible_input
+                if total_possible_input < total_out:
+                    warning = 'Warning: possible input value < output value {}.'.format(txid)
+                    print(warning)
+                    tx_issues.append(warning)
+
+            print('\tInputs known: {}/{}'.format(len(known_input_values), num_inputs))
+            itx['inputs_known'] = '{}/{}'.format(len(known_input_values), num_inputs)
+        else:
+            itx['inputs_known'] = 'repeated'
+
+        itx['outputs_known'] = '{}/{}'.format(num_known_outputs, num_outputs)
 
         print('\tOutputs known: {}/{}'.format(num_known_outputs, num_outputs))
         if num_outputs != num_known_outputs:
             for k, v in known_output_values.items():
                 print('\t\t', k, *v)
 
-            #for k, v in outputs_by_type.items():
-            #    print('\t\tKnown {}: {}/{}'.format(k, outputs_by_type[k], v))
-
-        #print('\toutputs_by_type', outputs_by_type)
-
-        #print('total_out', total_out)
-        #print('total_out', total_out)
-
         if total_out > total_in:
             warning = 'Warning: Mismatched value for tx: out > in {}.'.format(txid)
             print(warning)
-            issues.append(warning)
+            tx_issues.append(warning)
         elif total_in != total_out:
             warning = 'Warning: Mismatched value for tx: in != out {}.'.format(txid)
             print(warning)
-            issues.append(warning)
+            tx_issues.append(warning)
+
+        itx['issues'] = tx_issues
+        issues += tx_issues
 
         if spending_txid is not None:
-            return spent_out
+            return spent_out_values
         else:
             return claimed_outputs
 
@@ -270,11 +362,16 @@ def main():
     for itx in input_json['transactions']:
         print('')
         issues = []
-        outputs = trace_tx_inputs(itx, None, None, issues)
+        outputs = trace_tx_inputs(itx, None, '', None, issues)
         if len(issues) == 0:
             txns_likely_valid.append((itx['txid'], outputs))
         else:
             txns_check_further.append((itx['txid'], outputs))
+    print('')
+
+    print(json.dumps(input_json, indent=4))
+
+    print('Involved wallets:', len(wallet_names))
     print('')
 
     print('Likely valid txids:')
@@ -294,6 +391,8 @@ def main():
                      'blacklisted_aos': blacklisted_aos}
         json.dump(json_data, fp, indent=4)
 
+    if dbc:
+        dbc.close()
     print('Done.')
 
 
